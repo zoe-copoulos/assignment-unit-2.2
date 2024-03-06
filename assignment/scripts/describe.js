980ed0c2df1d98808b3961bba64bd97aa777268c
Matthew Black <matthewallenblack@gmail.com>
2024-03-06 11:01:59 -0600

    tweak instructions/formatting for clarity

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -1,18 +1,22 @@
 // WHAT IS THIS EVEN DOING?
 
-// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// For questions 1-3, each code block is syntactically and logically correct.
+// Your job is to describe what is happening. 
 // They are in isolation -- variables in one question do not affect other questions.
-// Describe the code as it would be run, and make sure you give us the console.log's value at the end.
-// (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
+// DESCRIBE the code as it would be run, then let us know what the OUTCOME of the
+// console.log statement will be.
 
 
-// 0. WRITE YOUR DESCRIPTION HERE
-// //EXAMPLE. We make a variable called number and set it to 1 as a number.
+// 0. (EXAMPLE!)
+// DESCRIPTION:
+// We make a variable called number and set it to 1 as a number.
 // Then we increment the number variable. Number is now 2.
-// We check if number is greater than or equal to 2. 2 is equal to 2, so
-// we console.log 'yes'.
+// We check if number is greater than or equal to 2. 2 is equal to 2, if
+// it is, we console.log 'yes'.
+// OUTCOME:
+// 'yes' is what is logged.
 
-//CODE
+// CODE:
 /*
 let number = 1;
 
@@ -21,17 +25,22 @@ number++;
 if (number >= 2) {
   console.log('yes');
 }
-
 */
 
+// ------------------------------------------------------------ //
+// ------------------------------------------------------------ //
 
-// 1. WRITE YOUR DESCRIPTION HERE
+
+// 1.
+// DESCRIPTION
 // 
 // 
 // 
 //
+// OUTCOME:
+//
 
-//CODE
+// CODE:
 /*
 let name = 'Dane';
 
@@ -40,17 +49,21 @@ if (name === 'Mary') {
 } else {
   console.log('How do you do?');
 }
-
-
 */
 
-// 2. WRITE YOUR DESCRIPTION HERE
+// ------------------------------------------------------------ //
+// ------------------------------------------------------------ //
+
+// 2.
+// DESCRIPTION:
 // 
 // 
 // 
 //
+// OUTCOME:
+//
 
-//CODE
+// CODE:
 /*
 let secret;
 
@@ -66,16 +79,21 @@ if (code > 250) {
 }
 
 console.log(secret)
-
 */
 
-// 3. WRITE YOUR DESCRIPTION HERE
+// ------------------------------------------------------------ //
+// ------------------------------------------------------------ //
+
+// 3.
+// DESCRIPTION
 // 
 // 
 // 
 //
+// OUTCOME:
+//
 
-//CODE
+// CODE:
 /*
 let isStudent = true;
 let age = 34;
@@ -93,7 +111,8 @@ if (isStudent === true && zip > 80000 ) {
 
 */
 
-
+// ------------------------------------------------------------ //
+// ------------------------------------------------------------ //
 
 
 // REVERSE
@@ -102,14 +121,14 @@ if (isStudent === true && zip > 80000 ) {
 // Comment above the problem code, and describe a potential solution.
 // Look closely -- things are logically incorrect or missing and wrong in the code!
 
-// EXAMPLE
-// 0. (DESCRIPTION OF CORRECT BEHAVIOR)
+// 0. (EXAMPLE!)
+// DESCRIPTION:
 // We make a variable called number and set it to 1 as a number.
 // Then we increment the number variable. Number is now 2.
 // We check if number is greater than or equal to 2. 
 // -- it is, so we console.log 'yes'
 
-//CODE
+// CODE:
 /*
 let number = 1;
 
@@ -120,15 +139,18 @@ number--;
 if (number >= 2) {
   console.log('yes');
 }
-
 */
 
+// ------------------------------------------------------------ //
+// ------------------------------------------------------------ //
 
-
-//4. We start with three variables -- colorOne is set to 'blue' and 
+// 4. 
+// DESCRIPTION:
+// We start with three variables -- colorOne is set to 'blue' and 
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
+// CODE:
 /*
 let colorOne = 'red';
 let colorTwo = 'blue';
@@ -139,10 +161,16 @@ if (mix === true) {
 }
 */
 
-//5. We start with two variables -- temp is set to 40 and time is set to 4.
+// ------------------------------------------------------------ //
+// ------------------------------------------------------------ //
+
+// 5.
+// DESCRIPTION:
+// We start with two variables -- temp is set to 40 and time is set to 4.
 // We check if temp is higher than 39 and if time is greater or equal to  4 
 // -- they are so we console.log 'throw away the food!'
 
+// CODE:
 /*
 let temp = 40;
 const time = 4;
@@ -152,10 +180,16 @@ if (temp > 39 || time >= 4) {
 }
 */
 
-//6. We start with two variables -- age is set to 21 and minAge is set to 21.
+// ------------------------------------------------------------ //
+// ------------------------------------------------------------ //
+
+// 6.
+// DESCRIPTION:
+// We start with two variables -- age is set to 21 and minAge is set to 21.
 // We check if age is greater than or equal to minAge 
 // -- it is, so we console.log 'enter'.
 
+// CODE:
 /*
 let age = 21;
 const minAge = 21;
@@ -166,4 +200,3 @@ if(minAge <= age) {
   console.log('enter');
 }
 */
-
